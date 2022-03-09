6c9547cee618dcfec5dc566dc65c80cfb19c11db
dannyp0930 <dannyp0930@gmail.com>
2022-03-09 18:23:03 +0900

    REFACTOR: Movie 컴포넌트 분리

---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import Proptypes from "prop-types"
+
+function Movie({coverImg, title, summary, genres}) {
+  return (
+    <div>
+      <img src={coverImg} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+Movie.propTypes = {
+  coverImg: Proptypes.string.isRequired,
+  title: Proptypes.string.isRequired,
+  summary: Proptypes.string.isRequired,
+  genres: Proptypes.arrayOf(Proptypes.string).isRequired,
+}
+export default Movie
