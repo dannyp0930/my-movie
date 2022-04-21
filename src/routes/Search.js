51fa05a90145c2542382385c403946691bf35966
dannyp0930 <dannyp0930@gmail.com>
2022-04-21 23:19:31 +0900

    FEAT: no search movies

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -9,6 +9,7 @@ export function Search() {
   const [ query, setQuery ] = useState('');
   const [ searchMovies, setSearchMovies ] = useState([]);
   const [ loading, setLoading ] = useState(false);
+  const [ noMovies, setNoMovies ] = useState(false);
 
   function handleChange(e) {
     setQuery(e.target.value)
@@ -19,25 +20,30 @@ export function Search() {
       setLoading(true);
       axios.get(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=ko-KR`)
         .then(res => {
-          setSearchMovies(res.data.results.map(movie => {
-            let posterPath = "null"
-            if (movie.poster_path) {
-              posterPath = movie.poster_path
-            }
-            return (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                posterPath={posterPath}
-                title={movie.title}
-                overview={movie.overview}
-              />
-            )
-          }))
+          if (res.data.results.length) {
+            setNoMovies(false);
+            setSearchMovies(res.data.results.map(movie => {
+              let posterPath = "null"
+              if (movie.poster_path) {
+                posterPath = movie.poster_path
+              }
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  posterPath={posterPath}
+                  title={movie.title}
+                  overview={movie.overview}
+                />
+              )
+            }))
+          } else {
+            setNoMovies(true);
+          }
+        });
       setTimeout(() => {
         setLoading(false);
       }, 300);
-      })
     } else {
       alert("검색어를 입력하세요.")
     }
@@ -56,7 +62,11 @@ export function Search() {
         <Loading />
       ) : (
         <Container>
-          {searchMovies.length ? searchMovies : <div>검색 결과가 없습니다.</div>}
+          {noMovies ? (
+            <div>검색 결과가 없습니다.</div>
+          ) : (
+            searchMovies
+          )}
         </Container>
       )}
     </div>
