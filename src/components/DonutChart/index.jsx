5b5888a4379c47bd6f8dc4636c051cc29d544820
dannyp0930 <dannyp0930@gmail.com>
2022-07-22 00:45:48 +0900

    FEAT: 도넛 차트 직접 구현

--- src/components/DonutChart/index.jsx
+++ src/components/DonutChart/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { PieChart } from "react-minimal-pie-chart";
 
 export default function DonutChart({ percentage }) {
-  function color(percentage) {
+  function getColor(percentage) {
     if (percentage <= 25) {
       return "#CF4421"
     } else if (percentage <= 50) {
@@ -12,33 +11,53 @@ export default function DonutChart({ percentage }) {
     } else {
       return "#21CF32"
     }
-  }
+  };
 
-  const data = [{
-    value: `${percentage}`,
-    color: color(percentage),
-    name: "name1",
-  }]
+  function getDrawColor(percentage) {
+    console.log(percentage / 100 * 360)
+    return percentage / 100 * 360
+  };
+
   return (
-    <PieChart
+    <div
       style={{
-        width: "10%",
         margin: "1rem"
       }}
-      data={data}
-      reveal={percentage}
-      lineWidth={30}
-      background="white"
-      lengthAngle={360}
-      rounded
-      animate
-      label={({ dataEntry }) => dataEntry.value}
-      labelStyle={{
-        fontSize: "2rem",
-        fontWeight: "bold",
-        fill: "white",
-      }}
-      labelPosition={0}
-    />
+    >
+      <svg
+        width="4rem" height="4rem"
+        viewBox="0 0 100 100"
+      >
+        <path
+          d = "M 92.5 50 A 42.5 42.5 0 1 1 92.5 49.9999"
+          fill="none"
+          stroke="white"
+          strokeWidth="15"
+          strokeLinecap="round"
+        />
+        <path
+          d = "M 92.5 50 A 42.5 42.5 0 1 1 92.5 49.9999"
+          fill="none"
+          stroke={getColor(percentage)}
+          strokeWidth="15"
+          strokeLinecap="round"
+          strokeDasharray={getDrawColor(percentage) - 10}
+          strokeDashoffset={370 - getDrawColor(percentage)}
+        />
+        <text
+          dominantBaseline="central"
+          x="50" y="50"
+          dx="0" dy="0"
+          textAnchor="middle"
+          style={{
+            fontSize: "2rem",
+            fontWeight: "bold",
+            fill: "white"
+          }}
+        >
+          {percentage}
+        </text>
+      </svg>
+    </div>
   )
 }
