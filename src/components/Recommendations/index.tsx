a45bacdeb53bcd76881024399ce2bd7dad35ae1a
dannyp0930 <dannyp0930@gmail.com>
2022-09-05 17:47:07 +0900

    FEAT: 추천 영화

--- src/components/Recommendations/index.tsx
+++ src/components/Recommendations/index.tsx
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
 import { Movie } from 'store/types/interfaces';
-import { API_KEY, BASE_URL } from 'utils/API';
+import { API_KEY, BASE_URL, IMG_URL } from 'utils/API';
+import { MovieContainer, MovieImg, MovieList } from './style';
+import DefatulBanner from "../../assets/images/default_banner.jpg";
 
 export default function Recommendations({ id }: { id: number }) {
   const [ recommendations, setRecommendations ] = useState<Movie[]>([]);
   useEffect(() => {
     async function getRecommendations() {
       const res = await axios.get(`${BASE_URL}/movie/${id}/recommendations?api_key=${API_KEY}&language=ko-KR`);
-      setRecommendations(res.data.results.slice(0, 10))
+      setRecommendations(res.data.results.slice(0, 8))
     };
     getRecommendations();
   }, [id]);
@@ -17,15 +18,16 @@ export default function Recommendations({ id }: { id: number }) {
   return (
     <div>
       <h2>추천 영화</h2>
-      {recommendations.map(movie => {
-        return (
-          <div key={movie.id}>
-            <Link to={`/movie/${movie.id}`}>
-              {movie.title}
-            </Link>
-          </div>
-        )
-      })}
+      <MovieList>
+        {recommendations.map(movie => {
+          return (
+            <MovieContainer to={`/movie/${movie.id}`} key={movie.id}>
+              <MovieImg src={movie.backdrop_path ? IMG_URL + movie.backdrop_path : DefatulBanner} alt=""/>
+              <p>{movie.title}</p>
+            </MovieContainer>
+          )
+        })}
+      </MovieList>
     </div>
   );
 };
--- src/components/Recommendations/style.ts
+++ src/components/Recommendations/style.ts
@@ -1,1 +1,30 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+export const MovieList = styled.div`
+  max-width: 25rem;
+  overflow-x: auto;
+  display: flex;
+
+  &::-webkit-scrollbar {
+    display: block;
+    width: 8px;
+    height: 8px;
+    border-radius: 6px;
+    background: rgba(255, 255, 255, 0.4);
+  };
+  &::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
+  };
+`;
+
+export const MovieContainer = styled(Link)`
+  padding: 1rem;
+  text-decoration: none;
+  color: white;
+`;
+
+export const MovieImg = styled.img`
+  height: 10rem;
+`;
